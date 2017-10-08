b869d06cb5802539779e1a4701e9bfb2248aa734
Kiyazasu <kenpo1469@gmail.com>
2017-10-08 16:49:34 +0800

    Integrated floor 2 to view. Floor viewing are reworked

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -27,6 +27,7 @@
 
         <script>
 
+        // Responsible for zooming and panning
         var svg = d3.select("svg")
             .append("svg")
             .attr("width", "100%")
@@ -38,14 +39,82 @@
             .on("dblclick.zoom", null)
             .append("g");
 
-        d3.xml("assets/groundfloor.svg")
-            .mimeType("image/svg+xml")
-            .get(function(error, xml) {
-                var g;
-                if (error) throw error;
-                g = xml.getElementsByTagName('svg')[0];
-                return svg.node().appendChild(g);
-            });
+        // Requests the chosen floor
+        function floor (level) {
+            // Clear any loaded or unwanted svg(s)
+            svg.selectAll("svg").transition().remove();
+            // Check and load the requested svg
+            if (level === 'level_g') {
+                // Retrieve floor G
+                retrieve('groundfloor.svg');
+            }
+            else if (level === 'level_1') {
+                // Retrieve floor 1
+                console.log("Hello, I am floor 1");
+            }
+            else if (level === 'level_2') {
+                // Retrieve floor 2
+                retrieve('secondfloor.svg');
+            }
+            else if (level === 'level_3') {
+                // Retrieve floor 3
+                console.log("Hello, I am floor 3");
+            }
+            else if (level === 'level_4') {
+                // Retrieve floor 4
+                console.log("Hello, I am floor 4");
+            }
+            else if (level === 'level_5') {
+                // Retrieve floor 5
+                console.log("Hello, I am floor 5");
+            }
+            else if (level === 'level_6') {
+                // Retrieve floor 6
+                console.log("Hello, I am floor 6");
+            }
+            else if (level === 'level_7') {
+                // Retrieve floor 7
+                console.log("Hello, I am floor 7");
+            }
+            else if (level === 'level_8') {
+                // Retrieve floor 8
+                console.log("Hello, I am floor 8");
+            }
+            else if (level === 'level_9') {
+                // Retrieve floor 9
+                console.log("Hello, I am floor 9");
+            }
+
+            // d3.selectAll('g')
+            //     .filter(function () { 
+            //         // Filter out 'g' elements without any ID tagged
+            //         return d3.select(this).attr('id');
+            //     })
+            //     .each(function () {
+            //         var currfloor = d3.select(this).transition();
+            //         if (this.id === level) { 
+            //             currfloor.style('opacity', 1) 
+            //         } else { 
+            //             currfloor.style('opacity', 0.01) 
+            //         }
+            //     }
+            // );
+        };
+
+        // Retrieves the SVG file
+        function retrieve (file) {
+            d3.xml("assets/" + file)
+                .mimeType("image/svg+xml")
+                .get(function(error, xml) {
+                    var g;
+                    if (error) throw error;
+                    g = xml.getElementsByTagName('svg')[0];
+                    return svg.node().appendChild(g);
+                }
+            );
+        }
+
+        floor('level_g'); // Default load floor G
 
         // Due to the document not loaded yet, this line of code will only 
         // execute once the above scripts are done loaded in the document.
@@ -75,25 +144,7 @@
                 console.log("L block was clicked!");
                 d3.select(this).style('fill', 'yellow');
             });
-
         });
-
-        function floor (level) {
-            d3.selectAll('g')
-                .filter(function () { 
-                    // Filter out 'g' elements without any ID tagged
-                    return d3.select(this).attr('id');
-                })
-                .each(function () {
-                    var currfloor = d3.select(this).transition();
-                    if (this.id === level) { 
-                        currfloor.style('opacity', 1) 
-                    } else { 
-                        currfloor.style('opacity', 0.01) 
-                    }
-                }
-            );
-        };
 
         </script>
 
