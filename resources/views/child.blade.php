b81ec595b20bb02a8c739c21b96d07b3c60feafb
iyujie <leslie.hahax@hotmail.com>
2017-10-30 12:55:41 +0800

    Revert "wip-heatmap-chart"
    This reverts commit 004c2d9dfa2ec8db19f4cc372e8f105dcd9a7ed9.

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
 
-<script src="http://colorbrewer2.org/export/colorbrewer.js">//color scaling for the temperature</script> 
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="/resources/demos/style.css">
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="https://d3js.org/d3.v4.min.js"></script>
-
     <br />
     <ul class="breadcrumb">
         <li><a href="#" onclick="floor('level_g')">Level G</a></li>
@@ -23,11 +16,7 @@
         <li><a href="#" onclick="floor('level_9')">Level 9</a></li>
     </ul>
 
-
     <svg preserveAspectRatio="xMidYMid meet"></svg>
-    <div id='HeatmapChartFilters'></div>
-    <div id='HeatmapChartApplyButton'></div>
-        
 
     <script>
 
@@ -103,273 +92,65 @@
         );
     }
 
+        floor('level_g'); // Default load floor G
 
+        // Due to the document not loaded yet, this line of code will only
+        // execute once the above scripts are done loaded in the document.
+        $(document).ready(function () {
 
+            // 'rect3769' = G block
+            // 'path4596' = A/B block
+            // 'path4598' = E block
+            // 'path4600' = L block
 
-    var heatmapLogs = null,
-        dateMin = null,
-        dateMax = null;
-
-
-
-    //Heatmap checkbox onclick function 
-    d3.select("#Heatmap").on("change",displayHeatmap);
-    displayHeatmap(heatmapLogs, dateMin, dateMax, 'All', 'All');
-
-    function displayHeatmap(heatmapLogs, dateMin, dateMax, floor, block){
-        if(d3.select("#Heatmap").property("checked")){
-            var heatmapLogs = {!! json_encode($heatmapLogs->toArray()) !!};
-
-            var parseDate = d3.timeParse("%Y-%m-%d");
-            var parseTime = d3.timeParse("%H:%M:%S");
-
-            heatmapLogs.forEach(function(d) {
-                if (floor.toUpperCase() == 'ALL' && block.toUpperCase() == 'ALL') {
-                    d.roomId = d.roomId;
-                    d.block = d.roomId[0].toUpperCase();
-                    d.floor = d.roomId[1];
-                    d.temperature = +d.temperature;
-
-                    if (d.date instanceof Date) && (d.time instanceof Date) {
-                        d.date = d.date;
-                        d.time = d.time;
-                    }
-                    else {
-                        d.date = parseDate(d.dateTime);
-                        d.time = parseTime(d.dateTime);
-                    }
-                }
-                if (floor.toUpperCase() == d.roomId[1] && block.toUpperCase() == 'ALL') {
-                    d.roomId = d.roomId;
-                    d.block = d.roomId[0].toUpperCase();
-                    d.floor = d.roomId[1];
-                    d.temperature = +d.temperature;
-
-                    if (d.date instanceof Date) && (d.time instanceof Date) {
-                        d.date = d.date;
-                        d.time = d.time;
-                    }
-                    else {
-                        d.date = parseDate(d.dateTime);
-                        d.time = parseTime(d.dateTime);
-                    }
-                }
-                if (floor.toUpperCase() == 'ALL' && block.toUpperCase() == d.roomId[0].toUpperCase()) {
-                    d.roomId = d.roomId;
-                    d.block = d.roomId[0].toUpperCase();
-                    d.floor = d.roomId[1];
-                    d.temperature = +d.temperature;
-
-                    if (d.date instanceof Date) && (d.time instanceof Date) {
-                        d.date = d.date;
-                        d.time = d.time;
-                    }
-                    else {
-                        d.date = parseDate(d.dateTime);
-                        d.time = parseTime(d.dateTime);
-                    }
-                }
-                else if(floor.toUpperCase() == d.roomId[1] && block.toUpperCase() == d.roomId[0].toUpperCase()){
-                    d.roomId = d.roomId;
-                    d.block = d.roomId[0].toUpperCase();
-                    d.floor = d.roomId[1];
-                    d.temperature = +d.temperature;
-
-                    if (d.date instanceof Date) && (d.time instanceof Date) {
-                        d.date = d.date;
-                        d.time = d.time;
-                    }
-                    else {
-                        d.date = parseDate(d.dateTime);
-                        d.time = parseTime(d.dateTime);
-                    }
-                }
-
+            d3.select("#rect3769").on("click", function () {
+                console.log("G block was clicked and turned red!");
+                d3.select(this).style('fill', 'red');
             });
 
+            d3.select("#path4596").on("click", function () {
+                console.log("A/B block was clicked!");
+                d3.select(this).style('fill', 'blue');
+            });
 
+            d3.select("#path4598").on("click", function () {
+                console.log("E block was clicked!");
+                d3.select(this).style('fill', 'green');
+            });
 
-            var data = d3.range(d.temperature);
-
-
-            // Set the range
-            var colors = d3.scaleLinear()
-                .domain([11.00,35.00])
-                .range(['#ffffd4','#fed98e','#fe9929','#d95f0e','#993404']);
-                    
-            var svg = d3.select("svg");
-            var rects = svg.selectAll(".rects")
-                .data(data)
-                .enter()
-                .append("rect")
-                .attr("fill", d=>colors(d));
-
-
-            createHeatmapChartFilters(heatmapLogs, dateMin, dateMax, 'All', 'All');
-            createHeatmapApplyButton(heatmapLogs);
-        }
-
-
-    }
-
-    function createHeatmapChartFilters(heatmapLogs, dateMin, dateMax, floor, block){
-
-        var dateSlider = "";
-
-        dateSlider += "<p>Date Range for Heatmap Chart : ";
-        dateSlider += "<input type='date-' id='date-HeatmapChart'>";
-        dateSlider += "</p>";
-        dateSlider += "<div id='dateSlider-securityChart' style='width:85%;margin: auto;'></div></br>";
-
-        var floorSelector = "";
-        floorSelector += "<p>Floor For Heatmap Chart : <select id='selectFloor-heatmapChart' size='1' style='width: 202px;'>";4
-        floorSelector += "<option value=All>All</option>";
-        floorSelector += "<option value=G>G</option>";
-        floorSelector += "<option value=1>1</option>";
-        floorSelector += "<option value=2>2</option>";
-        floorSelector += "<option value=3>3</option>";
-        floorSelector += "<option value=4>4</option>";
-        floorSelector += "<option value=5>5</option>";
-        floorSelector += "<option value=6>6</option>";
-        floorSelector += "<option value=7>7</option>";
-        floorSelector += "<option value=8>8</option>";
-        floorSelector += "<option value=9>9</option>";
-        floorSelector += "</select></p></br>";
-
-        var blockSelector = "";
-        blockSelector += "<p>Buildings For Heatmap Chart : <select id='selectBlock-heatmapChart' size='1' style='width: 202px;'>";4
-        blockSelector += "<option value=All>All</option>";
-        blockSelector += "<option value=A>A</option>";
-        blockSelector += "<option value=B>B</option>";
-        blockSelector += "<option value=E>E</option>";
-        blockSelector += "<option value=G>G</option>";
-        blockSelector += "<option value=L>L</option>";
-        blockSelector += "</select></p></br>";
-
-
-
-        document.getElementById('heatmapChartFilters').innerHTML = dateSlider + floorSelector + blockSelector;
-
-        tempData = [];
-
-        var securityLogs = {!! json_encode($securityLogs->toArray()) !!};
-
-        // parse the date / time
-        var parseTime = d3.timeParse("%Y-%m-%d");
-
-        // format the securityLogs
-        heatmapLogs.forEach(function(d) {
-            d.roomId = d.roomId;
-            d.block = d.roomId[0].toUpperCase();
-            d.floor = d.roomId[1];
-            d.temperature = +d.temperature;
-
-            if (d.date instanceof Date) {
-                d.date = d.date;
-            }
-            else {
-                d.date = parseTime(d.date);
-            }
+            d3.select("#path4600").on("click", function () {
+                console.log("L block was clicked!");
+                d3.select(this).style('fill', 'yellow');
+            });
         });
 
-        heatmapLogs.forEach(function (d){
-            tempData.push(d.date.getTime());
-        });
+        </script>
+        <script>
+            var securityLogs = {!! json_encode($securityLogs->toArray()) !!};
 
-        if (dateMin != null && dateMin != undefined && dateMax != null && dateMax != undefined) {
-            $(function (){
-                $("#dateSlider-heatmapChart").slider({
-                    range: true,
-                    min: Math.min.apply(null, tempData),
-                    max: Math.max.apply(null, tempData),
-                    values: [dateMin.getTime(), dateMax.getTime() ],
-                    slide: function( event, ui ) {
-                        dateMin = new Date(ui.values[0]);
-                        dateMax = new Date(ui.values[1]);
+            // parse the date / time
+    		var parseTime = d3.timeParse("%Y-%m-%d");
 
-                        $( "#date-heatmapChart").val(getFormattedDate(dateMin) + " - " + getFormattedDate(dateMax) );
-                    }
-                });
+    		// format the securityLogs
+    		securityLogs.forEach(function(d) {
+    			d.roomId = d.roomId;
+
+    			if (d.date instanceof Date) {
+    				d.date = d.date;
+    			}
+    			else {
+    				d.date = parseTime(d.date);
+    			}
+    			// d.date = d.date.getTime();
+    			d.transactionQuantity = +d.transactionQuantity;
+    		});
+
+            securityLogs.forEach(function (d){
+                    console.log(d);
             })
-        }
-        else {
-            $(function (){
-                $("#dateSlider-heatmapChart").slider({
-                    range: true,
-                    min: Math.min.apply(null, tempData),
-                    max: Math.max.apply(null, tempData),
-                    values: [Math.min.apply(null, tempData), Math.max.apply(null, tempData) ],
-                    slide: function( event, ui ) {
-                        var dateMin = new Date(ui.values[0]);
-                        var dateMax = new Date(ui.values[1]);
-
-                        $( "#date-heatmapChart").val(getFormattedDate(dateMin) + " - " + getFormattedDate(dateMax) );
-                    }
-                });
-            })
-        }
 
 
-
-    }
-
-
-    function createHeatmapApplyButton(heatmapLogs){
-
-        document.getElementById("heatmapChartApplyButton").innerHTML = "";
-
-        var heatmapChartApplyButton = document.createElement("heatmapChartApplyButton");
-
-        heatmapChartApplyButton.innerHTML = "Apply Filter";
-
-        document.getElementById("heatmapChartApplyButton").appendChild(heatmapChartApplyButton);
-
-        heatmapChartApplyButton.addEventListener ("click", function() {
-            console.log("---------- Submit Button Clicked ----------");
-            console.log($("#dateSlider-heatmapChart").val());
-            console.log($("#selectFloor-heatmapChart").val());
-            console.log($("#selectBlock-heatmapChart").val());
-
-            var min = new Date($("#dateSlider-heatmapChart").slider( "values", 0 )),
-                max = new Date($("#dateSlider-heatmapChart").slider( "values", 1 )),
-                floor = $("#selectFloor-heatmapChart").val(),
-                block = $("#selectBlock-heatmapChart").val();
-
-            document.getElementById("heatmapChart").innerHTML = "";
-
-            createHeatmapChart(heatmapLogs, min, max, floor, block)
-        });
-    }
-
-    function getFormattedDate(date) {
-        var year = date.getFullYear();
-
-        var month = (1 + date.getMonth()).toString();
-        month = month.length > 1 ? month : '0' + month;
-
-        var day = date.getDate().toString();
-        day = day.length > 1 ? day : '0' + day;
-
-        return day + '/' + month + '/' + year;
-    }
-
-
-
-
-    //Security checkbox onclick function 
-    d3.select("#Security").on("change",displaySecurity);
-    displaySecurity();
-    function displaySecurity(){
-        if(d3.select("#Security").property("checked")){
-            alert("This is alert box!");
-        }
-    }
-
-
-
-
-
-    </script>
-        
+            console.log(securityLogs);
+        </script>
 
 @endsection
