ff08a9d5b80d913803c2a7db2d12d1848b225cb6
banzaizai <ripariumnizer@gmail.com>
2017-11-15 14:18:08 +0800

    Cleaning up for security CSV

--- app/Http/Controllers/FileTransferController.php
+++ app/Http/Controllers/FileTransferController.php
@@ -9,38 +9,20 @@ use Input;
 
 class FileTransferController extends Controller
 {
-	public static function getFileName(Request $request)
-	{
-		$file = $request->file('file');
-		$roomID = basename($file->getClientOriginalName(),'.csv');
-		return $roomID;
-	}
-
-
-	public function securityDBupdate(Request $request)
-	{
-
-	}
 
 	public function uploadFile(Request $request)
 	{		
 			$file = $request->file('file');
-			$roomID = basename($file->getClientOriginalName(),'.csv');
-
 			
 			Excel::load($file, function($reader) 
 			{
-				$reader->formatDates(true, 'Y-m-d');
-
 				$reader->each(function($sheet)
 				{
 					$sheet->each(function($row)
 					{
 						$dbdata['roomId'] = $row['roomid'];
-
 						$dbdata['date'] = date('Y-m-d',strtotime($row['date']));
 						$dbdata['transactionQuantity'] = $row['transaction_quantity'];
-
 						
 						DB::table('security')->insert($dbdata);
 
