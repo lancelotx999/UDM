233f1635f3d9f47073f24dad57a5b99c116ad3f7
banzaizai <ripariumnizer@gmail.com>
2018-04-03 13:14:07 +0800

    CSV Upload WIP

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -12,24 +12,48 @@ class FileUploadController extends Controller
     {
     	if ($request->file('file')->isValid())
     	{
-    		$file = $request->file('file');
-            $filepath = $file->getPathName();
-
   			$request->validate([
     			'file' => 'required|mimes:csv,txt,xlsx'
     		]);
+    	}
 
-            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($filepath);
-            $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-            dd($sheetData);
+    	$file = $request->file('file');
+        $filepath = $file->getPathName();
 
-        //    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
-        //    $spreadsheet = $reader->load($file);
+		$finfo = finfo_open(FILEINFO_MIME_TYPE);
+		$filemime = finfo_file($finfo,$filepath);
+
+		//dd($filemime);
+
+		if (($filemime == "text/plain" ) || ($filemime == "text/csv"))
+		{
+			return $this->readCSV($filepath);
+		}
+		else if (($filemime == "application/vnd.ms-excel") || ($filemime == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+		{
+			return $this->readXLSX($filepath);
+		}
 
 
+        
+       // $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($filepath);
+       // $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+    }
 
-    		
-    	} 	
+    public function readCSV($filepath)
+    {
+    	$inputFileType = 'Csv';
+    	$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+
+    	$spreadsheet = $reader->load($filepath);
+    	dd($spreadsheet);
+
+
+    }
+
+    public function readXLSX($filepath)
+    {
+    	dd("swag3");
     }
 
 }
