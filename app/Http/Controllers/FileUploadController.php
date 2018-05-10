1c43191d7262aa7c93ac5a1fdcfea0fccb8205b0
banzaizai <ripariumnizer@gmail.com>
2018-05-11 02:19:46 +0800

    Removed dependency on 3rd party lib

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -5,19 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Reader\IReadFilter;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
 
-class FileUploadController extends Controller implements IReadFilter 
+class FileUploadController extends Controller 
 {
-	private $startRow = 0;
-	private $endRow = 0;
-
     public function UploadtoServer(Request $request)
     {
     	if ($request->file('file')->isValid())
@@ -37,85 +30,47 @@ class FileUploadController extends Controller implements IReadFilter
 
 		if (($filemime == "text/plain" ) || ($filemime == "text/csv"))
 		{
-			return $this->readCSV($filepath);
+			return $this->importCSV($filepath,$filename);
 		}
-		else if (($filemime == "application/vnd.ms-excel") || ($filemime == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
-		{
-			return $this->readXLSX($filepath);
-		}
+
     }
 
-    public function setRows($startRow, $chunkSize)
+    public function importCSV($filepath, $filename)
     {
-        $this->startRow = $startRow;
-        $this->endRow = $startRow + $chunkSize;
-    }
-
-    public function readCell($column, $row, $worksheetName = '')
-    {
-        if (($row == 1) || ($row >= $this->startRow && $row < $this->endRow))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    public function readCSV($filepath)
-    {
-    	$inputFileType = 'Csv';
-    	$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
     	$chunkSize = 100;
-    	$chunkFilter = new FileUploadController();
-    	$reader->setReadFilter($chunkFilter)->setContiguous(true);
 
-    	$spreadsheet = new Spreadsheet();
-    	$sheet = 0;
-
-    	for ($startRow = 2; $startRow <= 240; $startRow += $chunkSize) 
+    	$filehandle = fopen($filepath, "r");
+    	if ($filehandle === FALSE)
     	{
-       		$chunkFilter->setRows($startRow, $chunkSize);
+    		dd("File doesn't exist/ can't be accessed");
+    	}
 
-    		$reader->setSheetIndex($sheet);
-    		$reader->loadIntoExisting($filepath, $spreadsheet);
-    		$spreadsheet->getActiveSheet()->setTitle('Sheet #' . (++$sheet));
-		}
+    	//Get Headers
+    	fseek($filehandle,0);
+    	$headers = (fgetcsv($filehandle));
 
-		$loadedSheetNames = $spreadsheet->getSheetNames();
-
-		
-		foreach ($loadedSheetNames as $sheetIndex => $loadedSheetName) 
+    	$offset = 0;
+    	while(!feof($filehandle))
 		{
-    		$spreadsheet->setActiveSheetIndexByName($loadedSheetName);
-    		$sheetData = $spreadsheet->getActiveSheet()->toArray(null, false, false, true);
+    		fseek($filehandle, $offset);
 
-    		if ($sheetIndex == 0)
-    		{
-    			$headers = array_values($sheetData)[0];
-    		}
-
-		}
-
-		if (Schema::hasTable($filepath)) 
-    	{
-    		dd("swag1");
-		}
-		else
-		{
-    		Schema::create($filepath, function (Blueprint $table) use ($headers)
-    		{
-    			foreach ($headers as $header)
+    		$i = 0;
+    			while (($currRow = fgetcsv($filehandle)) !== FALSE)
     			{
-    				$table->string($header);
+        			$i++; 
+        			print implode(', ', $currRow)."\n";
+        			if($i >= $chunkSize)
+        			{
+            			$offset = ftell($filehandle);
+            			break;
+        			}
     			}
-			});
 		}
 
+		fclose($filehandle);
+
     }
 
-    public function readXLSX($filepath)
-    {
-    	dd("swag3");
-    }
 
 
 }
