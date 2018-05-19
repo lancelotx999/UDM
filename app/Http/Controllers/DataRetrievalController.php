b4ca53c76733493013fdee1cbfa4584f892dbeb9
banzaizai <ripariumnizer@gmail.com>
2018-05-19 21:29:44 +0800

    Composer Update

---
+++ app/Http/Controllers/DataRetrievalController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+
+class DataRetrievalController extends Controller 
+{
+   
+}
+ 
