dc1ea3eef8e82f68b35f59c23514634016e94c07
Kiyazasu <kenpo1469@gmail.com>
2018-05-19 23:31:53 +0800

    Fixed missing home controller, editor work in progress

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -7,14 +7,27 @@
 </div>
 
 <script>
-    var map = L.map('map').setView([-41.2858, 174.78682], 14);
-    mapLink = 
-        '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-    L.tileLayer(
-        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; ' + mapLink + ' Contributors',
-        maxZoom: 18,
-        }).addTo(map);
+    var map = L.map('map', { zoomControl: false })
+        // .setView([0, 0], 0)
+        .setView(new L.LatLng(40, -74), 10) //NYC Location
+        .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+
+    //add zoom control with your options
+    L.control.zoom({ position:'topright' }).addTo(map);
+
+    map.options.minZoom = 2;
+
+    var southWest = new L.LatLng(-90, 180);
+    var northEast = new L.LatLng(90, -180);
+
+    var colors = ['#ff4b00', '#bac900', '#EC1813', '#55BCBE', '#D2204C', '#FF0000', '#ada59a', '#3e647e'],
+        pi2 = Math.PI * 2;
+
+    map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+
+    map.fitBounds(new L.LatLngBounds(southWest, northEast), {reset: true});
+
+    map.setView(new L.LatLng(40.7, -73.8), 10) //NYC Location
 
     var drawnItems = new L.FeatureGroup();
     map.addLayer(drawnItems);
