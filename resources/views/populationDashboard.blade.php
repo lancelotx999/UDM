d6ea7037a76e814ec8766af6c4113efebc02a9b5
Kiyazasu <kenpo1469@gmail.com>
2018-06-09 15:07:58 +0800

    Polishing dashboard

--- resources/views/populationDashboard.blade.php
+++ resources/views/populationDashboard.blade.php
@@ -38,47 +38,35 @@
     	<div class="clearfix"></div>
 
     	<div class="row">
-      		<div class="col-md-12 col-sm-12 col-xs-12">
+      		<div class="col-md-6 col-sm-6 col-xs-12">
         		<div class="x_panel">
           			<div class="x_title" >
-            			<h3>Population In New York</h3>
+            			<h3>Population Statistics by Boroughs</h3>
             			<ul class="nav navbar-right panel_toolbox">
 	              			<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-	              			<!-- <li class="dropdown">
-	                			<a href="#" classh="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-	                			<ul class="dropdown-menu" role="menu">
-	                  				<li><a href="#">Settings 1</a></li>
-	                  				<li><a href="#">Settings 2</a></li>
-	                			</ul>
-	              			</li> -->
             			</ul>
             			<div class="clearfix"></div>
           			</div>
-          			<div class="x_content">
-						<strong>Population Statistics by Boroughs</strong>
+					<div id='populationByBoroughChart'></div>					
+					<span id='populationByBoroughChartFilters'></span>
+					<span id='populationByBoroughChartApplyButton'></span>
+					<span id='populationByBoroughChartResetButton'></span>
+        		</div>
+      		</div>
 
-						<hr />
-
-						<div id='populationByBoroughChart'></div>
-
-						<div class="row">
-							<span id='populationByBoroughChartFilters'></span>
-							<span id='populationByBoroughChartApplyButton'></span>
-							<span id='populationByBoroughChartResetButton'></span>
-						</div>
-						<strong>Population Statistics by Community District In A Borough</strong>
-
-						<hr />
-
-						<div id='populationByCommunityChart'></div>
-
-						<div class="row">
-							<span id='populationByCommunityChartFilters'></span>
-							<span id='populationByCommunityChartApplyButton'></span>
-							<span id='populationByCommunityChartResetButton'></span>
-						</div>
-            			<!-- <div id="mainb" style="height:350px;"></div> -->
+      		<div class="col-md-6 col-sm-6 col-xs-12">
+        		<div class="x_panel">
+          			<div class="x_title">
+            			<h3>Population Statistics by Community District In A Borough</h3>
+            			<ul class="nav navbar-right panel_toolbox">
+              				<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+            			</ul>
+            			<div class="clearfix"></div>
           			</div>
+					<div id='populationByCommunityChart'></div>
+					<span id='populationByCommunityChartFilters'></span>
+					<span id='populationByCommunityChartApplyButton'></span>
+					<span id='populationByCommunityChartResetButton'></span>
         		</div>
       		</div>
       	</div>
